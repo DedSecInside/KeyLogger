f737643828de481779f6dfc264300606932bfb57
PSNAppZ <psnappz@gmail.com>
2015-06-23 17:31:28 +0530

    V1.0(Added Registrtion Error Message)

--- KeyLogger/Menu.vb
+++ KeyLogger/Menu.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Menu
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If TextBox1.Text = "" Then
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
             Errorlbl.Text = " Please Fill all the above Fields to Register!"
         End If
     End Sub
